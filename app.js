ce17a68de1c7f9642b237baf1d02ad516afbcf41
Joseph397 <gramkracker88@gmail.com>
2020-08-21 14:58:19 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Arrays and for loops
+
+const names =['anna', 'susy', 'bob', 'john', 'susan'];
+const lastName = 'shakeandbake';
+let newArray = [];
+
+//for loop
+
+for(let i = 0; i < names.length; i++) {
+    console.log(i);
+    console.log(names[i]);
+const fullName = ``
+    newArray.push(`${names[i]} ${lastName}`);
+}
+
+console.log(names);
+console.log(newArray);
